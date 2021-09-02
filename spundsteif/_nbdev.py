e158a51cd3c4ded0c256f4d7f79552f8c7f92b51
PAYEUR <jeanbaptistepayeur@gmail.com>
2021-09-02 10:05:24 +0000

    Write 01sensors.ipynb

--- spundsteif/_nbdev.py
+++ spundsteif/_nbdev.py
@@ -2,9 +2,29 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"__construct_INFO__": "01_sensors.ipynb",
+         "__construct_list_DATA__": "01_sensors.ipynb",
+         "read_files": "01_sensors.ipynb",
+         "__dates_sorted__": "01_sensors.ipynb",
+         "__ind_chrono_order__": "01_sensors.ipynb",
+         "sort_chrono": "01_sensors.ipynb",
+         "__elements_vides__": "01_sensors.ipynb",
+         "traitement_elements_vides": "01_sensors.ipynb",
+         "__condition_DATA__": "01_sensors.ipynb",
+         "list_names_channels": "01_sensors.ipynb",
+         "extract_T0": "01_sensors.ipynb",
+         "construction_list_df_messung": "01_sensors.ipynb",
+         "concat_df_messung": "01_sensors.ipynb",
+         "traitement_colonnes_zeit": "01_sensors.ipynb",
+         "__clean_name__": "01_sensors.ipynb",
+         "traitement_colonnes_names": "01_sensors.ipynb",
+         "conversion_float": "01_sensors.ipynb",
+         "df_to_m": "01_sensors.ipynb",
+         "define_index": "01_sensors.ipynb",
+         "creation_df_hand": "01_sensors.ipynb",
+         "run_computation": "01_sensors.ipynb"}
 
-modules = []
+modules = ["sensors.py"]
 
 doc_url = "https://PAYEUR.github.io/spundsteif/"
 
